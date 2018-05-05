77615e0d45b4a6a35dae73ea85f3ec23ae40e3c6
Chetan Solanki <chetan.solanki@pervacio.com>
2018-05-05 22:35:38 +0530

    taviscaDemoTest

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  id = "";
+  hotelList : any[] = 
+  [
+    {
+      "name" : "HotelName1",
+      "location" : "New York",
+      "detailAddress" : "Road number 123 New York",
+      "currency" : "$",
+      "price" : 136,
+    },
+    {
+      "name" : "HotelName2",
+      "location" : "Washington",
+      "detailAddress" : "Road number 123 Washington",
+      "currency" : "$",
+      "price" : 79,
+    },
+    {
+      "name" : "HotelName3",
+      "location" : "Las Vegas",
+      "detailAddress" : "Road number 123 Las Vegas",
+      "currency" : "$",
+      "price" : 256,
+    },
+    {
+      "name" : "HotelName4",
+      "location" : "Texas",
+      "detailAddress" : "Road number 123 Texas",
+      "currency" : "$",
+      "price" : 204,
+    },
+  ]
 }
